112738ed1744db689a3ee0c859e4ae1c08c46e16
shiorioz <ozws011.tmt@gmail.com>
2024-01-14 14:25:13 +0900

    feat: 色の定義を追加する

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Constant {
+  static Color blue = const Color(0xFF93AEC1).withOpacity(0.9);
+  static Color red = const Color(0xFFEC6A52);
+  static const Color darkGray = Color(0xFF343434);
+  static const Color lightGray = Color(0xFFD9D9D9);
+}
